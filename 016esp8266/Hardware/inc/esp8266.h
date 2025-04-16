28105a99fa4f16cb5adc97067c78e2b4b6f6e699
Arosy <hzu5@unionman.com.cn>
2025-04-16 11:30:56 +0800

    fix

--- 016esp8266/Hardware/inc/esp8266.h
+++ 016esp8266/Hardware/inc/esp8266.h
@@ -8,6 +8,8 @@
 #define RST_DOWN HAL_GPIO_WritePin(ESPCS_GPIO_Port, ESPCS_Pin, GPIO_PIN_SET)
 #define RST_UP HAL_GPIO_WritePin(ESPCS_GPIO_Port, ESPCS_Pin, GPIO_PIN_RESET)
 
+#define ESPCS_Pin GPIO_PIN_8
+#define ESPCS_GPIO_Port GPIOF
 
 void espSendData(uint8_t *data); 
 void espSendDatas(_Bool IsWificonfig);
